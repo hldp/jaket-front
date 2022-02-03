e5ff04051b93dd2f881c6a4d93a18496caab4092
Nathanha <haro.nathan@gmail.com>
2022-02-03 10:25:55 +0100

    Added router

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="crossOrigin=""/>
-    <link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet"></link>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="crossOrigin=""/>
+      <link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet"></link>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
